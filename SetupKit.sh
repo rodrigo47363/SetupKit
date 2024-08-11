213f0b6a2974a94e900231b6c9fe1577b8abf288
rodrigo47363 <157255595+rodrigo47363@users.noreply.github.com>
2024-08-11 12:12:16 -0600

    Update SetupKit.sh

--- SetupKit.sh
+++ SetupKit.sh
@@ -11,8 +11,9 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 RESET="\e[0m"
 
-# Directorio de instalación por defecto para temas personalizados de Oh-My-Zsh
+# Directorios de instalación por defecto
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+BASH_CUSTOM="${BASH_CUSTOM:-$HOME/.oh-my-bash/custom}"
 
 # Función para mostrar mensajes de error y salir
 error_exit() {
@@ -78,43 +79,56 @@ install_packages() {
         hcxdumptool hcxtools lsd || error_exit "No se pudieron instalar los paquetes necesarios"
 }
 
-# Función para instalar Oh-My-Zsh
+# Función para instalar Oh-My-Zsh para el usuario especificado
 install_oh_my_zsh() {
-    echo -e "${YELLOW}Instalando Oh-My-Zsh...${RESET}"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || error_exit "No se pudo instalar Oh-My-Zsh"
+    echo -e "${YELLOW}Instalando Oh-My-Zsh para el usuario ${1}...${RESET}"
+    local user="$1"
+    sudo -u "$user" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || error_exit "No se pudo instalar Oh-My-Zsh para ${user}"
 }
 
-# Función para instalar Powerlevel10k
+# Función para instalar Powerlevel10k para el usuario especificado
 install_powerlevel10k() {
-    echo -e "${YELLOW}Instalando Powerlevel10k...${RESET}"
-    [ ! -d "${ZSH_CUSTOM}/themes" ] && mkdir -p "${ZSH_CUSTOM}/themes"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k || error_exit "No se pudo instalar Powerlevel10k"
+    echo -e "${YELLOW}Instalando Powerlevel10k para el usuario ${1}...${RESET}"
+    local user="$1"
+    local zsh_custom_dir=$(sudo -u "$user" bash -c 'echo $ZSH_CUSTOM')
+    [ ! -d "${zsh_custom_dir}/themes" ] && sudo -u "$user" mkdir -p "${zsh_custom_dir}/themes"
+    sudo -u "$user" git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${zsh_custom_dir}/themes/powerlevel10k || error_exit "No se pudo instalar Powerlevel10k para ${user}"
 }
 
-# Función para descargar e instalar fuentes Hack Nerd Fonts
+# Función para descargar e instalar fuentes Hack Nerd Fonts para el usuario especificado
 install_nerd_fonts() {
-    echo -e "${YELLOW}Descargando e instalando fuentes Hack Nerd Fonts...${RESET}"
+    echo -e "${YELLOW}Descargando e instalando fuentes Hack Nerd Fonts para el usuario ${1}...${RESET}"
+    local user="$1"
     FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip"
-    FONT_DIR="$HOME/.local/share/fonts"
-    FONT_FILE="/tmp/hack.zip"
+    local font_dir=$(sudo -u "$user" bash -c 'echo $HOME/.local/share/fonts')
+    local font_file="/tmp/hack.zip"
 
-    wget -O "$FONT_FILE" "$FONT_URL" || error_exit "No se pudo descargar el archivo de fuentes"
-    unzip "$FONT_FILE" -d "$FONT_DIR" || error_exit "No se pudo descomprimir el archivo de fuentes"
-    rm "$FONT_FILE" || error_exit "No se pudo eliminar el archivo temporal"
-    fc-cache -fv || error_exit "No se pudo actualizar la caché de fuentes"
+    sudo -u "$user" wget -O "$font_file" "$FONT_URL" || error_exit "No se pudo descargar el archivo de fuentes"
+    sudo -u "$user" unzip "$font_file" -d "$font_dir" || error_exit "No se pudo descomprimir el archivo de fuentes"
+    rm "$font_file" || error_exit "No se pudo eliminar el archivo temporal"
+    sudo -u "$user" fc-cache -fv || error_exit "No se pudo actualizar la caché de fuentes"
 }
 
-# Función para configurar Zsh y Oh-My-Zsh
+# Función para configurar Zsh y Oh-My-Zsh para el usuario especificado
 configure_zsh() {
-    echo -e "${YELLOW}Configurando Zsh y Oh-My-Zsh...${RESET}"
-    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc || error_exit "No se pudo configurar el tema de Oh-My-Zsh"
-    sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc || error_exit "No se pudo configurar los plugins de Oh-My-Zsh"
+    echo -e "${YELLOW}Configurando Zsh y Oh-My-Zsh para el usuario ${1}...${RESET}"
+    local user="$1"
+    sudo -u "$user" sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' /home/$user/.zshrc || error_exit "No se pudo configurar el tema de Oh-My-Zsh para ${user}"
+    sudo -u "$user" sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' /home/$user/.zshrc || error_exit "No se pudo configurar los plugins de Oh-My-Zsh para ${user}"
 }
 
-# Función para cambiar la shell por defecto a Zsh
+# Función para cambiar la shell por defecto a Zsh para el usuario especificado
 change_default_shell() {
-    echo -e "${YELLOW}Cambiando la shell por defecto a Zsh...${RESET}"
-    chsh -s $(which zsh) || error_exit "No se pudo cambiar la shell por defecto a Zsh"
+    echo -e "${YELLOW}Cambiando la shell por defecto a Zsh para el usuario ${1}...${RESET}"
+    local user="$1"
+    sudo -u "$user" chsh -s $(which zsh) || error_exit "No se pudo cambiar la shell por defecto a Zsh para ${user}"
+}
+
+# Función para instalar Oh-My-Bash para el usuario especificado
+install_oh_my_bash() {
+    echo -e "${YELLOW}Instalando Oh-My-Bash para el usuario ${1}...${RESET}"
+    local user="$1"
+    sudo -u "$user" bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" || error_exit "No se pudo instalar Oh-My-Bash para ${user}"
 }
 
 # Mensaje de bienvenida
@@ -128,10 +142,21 @@ echo -e "${GREEN}Bienvenido a la automatización de personalización del entorno
 # Ejecución de funciones
 install_deps
 install_packages
-install_oh_my_zsh
-install_powerlevel10k
-install_nerd_fonts
-configure_zsh
-change_default_shell
 
-echo -e "${GREEN}¡La personalización de tu entorno ha sido completada con éxito!${RESET}"
+# Instalación para el usuario root
+install_oh_my_zsh root
+install_powerlevel10k root
+install_nerd_fonts root
+configure_zsh root
+change_default_shell root
+install_oh_my_bash root
+
+# Instalación para el usuario actual
+install_oh_my_zsh "$(whoami)"
+install_powerlevel10k "$(whoami)"
+install_nerd_fonts "$(whoami)"
+configure_zsh "$(whoami)"
+change_default_shell "$(whoami)"
+install_oh_my_bash "$(whoami)"
+
+echo -e "${GREEN}¡La instalación y configuración se completó con éxito!${RESET}"
